4259153a5976cbd51a08a7669aedcab02643c279
Mujle <sasha.kosanovic@gmail.com>
2020-12-25 21:53:59 +0100

    Added missing functionalities for the teacher api

--- src/main/java/com/alasdoo/developercourseassignment/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/alasdoo/developercourseassignment/service/impl/TeacherServiceImpl.java
@@ -1,37 +1,88 @@
 package com.alasdoo.developercourseassignment.service.impl;
 
 import com.alasdoo.developercourseassignment.dto.TeacherDTO;
+import com.alasdoo.developercourseassignment.entity.Teacher;
+import com.alasdoo.developercourseassignment.mapper.TeacherMapper;
+import com.alasdoo.developercourseassignment.repository.TeacherRepository;
+import com.alasdoo.developercourseassignment.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-public class TeacherServiceImpl {
+public class TeacherServiceImpl implements TeacherService {
 
+    //Added implementation for missing method bodies
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Autowired
+    private TeacherMapper teacherMapper;
+
+    @Override
     public TeacherDTO findOne(Integer id) {
-        return null;
+        Optional<Teacher> teacher = teacherRepository.findById(id);
+        if (!teacher.isPresent()) {
+            throw new IllegalArgumentException
+                    ("Teacher with the following id = " + id + " was not found.");
+        }
+        return teacherMapper.transformToDTO(teacher.get());
     }
 
+    @Override
     public List<TeacherDTO> findAll() {
-        return null;
+        return teacherRepository.findAll().stream().map(i -> teacherMapper.transformToDTO(i)).collect(Collectors.toList());
     }
 
+    @Override
     public TeacherDTO save(TeacherDTO teacherDTO) {
-        return null;
+        Teacher teacher = teacherMapper.transformToEntity(teacherDTO);
+        return teacherMapper.transformToDTO(teacherRepository.save(teacher));
     }
 
+    @Override
     public void remove(Integer id) throws IllegalArgumentException {
+        Optional<Teacher> teacher = teacherRepository.findById(id);
+        if (!teacher.isPresent()) {
+            throw new IllegalArgumentException
+                    ("Teacher with the following id = " + id + " was not found.");
+        }
+        teacherRepository.deleteById(id);
     }
 
+    @Override
     public TeacherDTO update(Integer id, TeacherDTO teacherDTO) {
-        return null;
+        Optional<Teacher> oldTeacher = teacherRepository.findById(id);
+        if (!oldTeacher.isPresent()) {
+            throw new IllegalArgumentException
+                    ("Teacher with the following id = " + id + " was not found.");
+        }
+        oldTeacher.get().setTeacherName(teacherDTO.getTeacherName());
+        oldTeacher.get().setTeacherSurname(teacherDTO.getTeacherSurname());
+        oldTeacher.get().setTeacherEmail(teacherDTO.getTeacherEmail());
+        teacherRepository.save(oldTeacher.get());
+        return teacherMapper.transformToDTO(oldTeacher.get());
     }
 
     public TeacherDTO findByTeacherNameAndTeacherSurname(String name, String surname) {
-        return null;
+        Optional<Teacher> teacher = teacherRepository.findByTeacherNameAndTeacherSurname(name, surname);
+        if (!teacher.isPresent()) {
+            throw new IllegalArgumentException
+                    ("Teacher with the provided name and surname was not found");
+        }
+        return teacherMapper.transformToDTO(teacher.get());
     }
 
     public TeacherDTO findByTeacherEmail(String email) {
-        return null;
+        Optional<Teacher> teacher = teacherRepository.findByTeacherEmail(email);
+        if (!teacher.isPresent()) {
+            throw new IllegalArgumentException
+                    ("Teacher with the following email = " + email + " was not found" );
+        }
+        return teacherMapper.transformToDTO(teacher.get());
     }
 }
