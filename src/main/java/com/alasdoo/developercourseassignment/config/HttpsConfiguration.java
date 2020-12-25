1110d3276923dda7e28a82bd7b00863e40a5c1dc
Mujle <sasha.kosanovic@gmail.com>
2020-12-25 19:59:19 +0100

    Enabled SSL on backend

---
+++ src/main/java/com/alasdoo/developercourseassignment/config/HttpsConfiguration.java
@@ -0,0 +1,31 @@
+package com.alasdoo.developercourseassignment.config;
+
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class HttpsConfiguration {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        //Enable SSL Traffic
+        return new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+    }
+
+}
